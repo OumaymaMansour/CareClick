f2a268389ad9551e85404eafb81a095e65cde8ae
aminelaarif <laarifamine5569@gmail.com>
2024-04-20 17:26:59 +0100

    created navigation page

--- Client/App.tsx
+++ Client/App.tsx
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Signin from './Signin';
+import Signup from './Signup';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Signin" component={Signin} />
+        <Stack.Screen name="Signup" component={Signup} />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
