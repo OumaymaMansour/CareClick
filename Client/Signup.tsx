d706d7c31536f4498305865c0279736912fb9525
aminelaarif <laarifamine5569@gmail.com>
2024-04-20 17:29:06 +0100

    created sign up page

---
+++ Client/Signup.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { View,Image,StyleSheet} from 'react-native'
+function Signup() {
+  
+  return (
+    <View>
+      <Image
+        
+        source={require('./assets/logo.png')}
+        />
+    </View>
+  )
+}
+
+export default Signup
+
+
+
+
+
+
+
+
